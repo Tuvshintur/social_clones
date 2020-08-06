881d92b113643640e5b1089885d8fb30b36001e5
Tuvshintur Batzorig <tuvshinturbatzorig@gmail.com>
2020-08-06 14:15:43 -0500

    instagram

---
+++ instagram-clone/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="app">
+            <div className="app__header">
+                <img
+                    className="app__headerImage"
+                    src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                    alt=""
+                />
+            </div>
+        </div>
+    );
+}
+
+export default App;
